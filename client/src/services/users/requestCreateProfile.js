40f5f5ff0d45b4143f08a060b367ace076eb3a65
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-04-02 19:08:31 -0500

    RequestCreateProfile

--- client/src/services/users/requestCreateProfile.js
+++ client/src/services/users/requestCreateProfile.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-const  requestCreateProfile = async (token, profileData) => {
+const requestCreateProfile = async (token, profileData) => {
   try {
     const response = await axios.post(
-      "http://localhost:4000/auth/profile/:userId",
+      `http://localhost:4000/auth/profile/${profileData.userId}`,
       profileData,
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-        }
+      }
     );
     return response.data;
-    }
-    catch (error) {
+  } catch (error) {
     console.error("Error al crear perfil:", error);
     throw error;
-    }
-}
+  }
+};
 
 export default requestCreateProfile;
