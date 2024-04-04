8908fd05eaa225301ea64933484cfed7bf15ff23
Arreghini <marreghi@gmail.com>
2024-04-04 01:53:22 -0300

    offers and services

--- client/src/components/roomServices/Services.jsx
+++ client/src/components/roomServices/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
               <img src={service.Url} alt={service.name} className="w-full h-full object-cover" />
             </div>
           </div>
-          <div className="w-full lg:w-1/2  p-4 flex flex-col justify-center gap-4">
+          <div className="w-full lg:w-1/2 p-4 flex flex-col justify-center gap-4">
             <div>
               <h1 className='text-3xl lg:text-6xl text-center font-bold mt-0 mb-6'>{service.name}</h1>
               <p className="text-lg lg:text-3xl leading-relaxed mb-4">{service.description}</p>
@@ -21,18 +21,17 @@ const Services = () => {
                   <li key={index} className="text-base lg:text-3xl">{serviceItem}</li>
                 ))}
               </ul>
-              {index === 2 && (
+              {index === 2 ? (
                 <div className="flex justify-center">
-                  <Link to='/restaurant'>
-                    <button className="border border-gray-400 bg-amber-300 hover:bg-amber-400 transition-colors text-black font-bold py-2 px-4 rounded w-40 h-12">
+                  <Link to='/restaurant' className='w-1/4 mx-4'>
+                    <button className="text-xl py-4 font-bold text-white bg-amber-300 hover:bg-amber-400 transition-colors rounded-2xl shadow-lg w-full">
                       BOOK
                     </button>
                   </Link>
                 </div>
-              )}
-              {index !== 2 && (
-                <div className="flex justify-center items-center">
-                  <div className="border border-gray-400 bg-d rounded-md px-4 py-2 text-center w-40">
+              ) : (
+                <div className="flex justify-center">
+                  <div className="text-xl py-4 font-bold text-white bg-amber-300 hover:bg-amber-400 transition-colors rounded-2xl shadow-lg w-1/4 text-center">
                     {service.price}
                   </div>
                 </div>
