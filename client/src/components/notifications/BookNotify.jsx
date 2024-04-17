b9d1f909150d1c8193610f7b734b15c86ded72af
Arreghini <marreghi@gmail.com>
2024-04-17 04:10:44 -0300

    socket.io prueba

--- client/src/components/notifications/BookNotify.jsx
+++ client/src/components/notifications/BookNotify.jsx
@@ -24,7 +24,7 @@ const BookNotify = ({ socket }) => {
       setConfirmationMessage('Failed to send notification. Please try again later.');
     }
   };
-
+ // a
   return (
     <div className="max-w-4xl mx-auto my-8 px-4">
       <h2 className="text-2xl font-bold mb-6 text-center">Book Notification</h2>
