ce5a5003f297eba8c19b33b60cdb960b43391526
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-21 18:32:50 -0600

    cliente funcionando

--- src/components/chat/ClientChat.jsx
+++ src/components/chat/ClientChat.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
 import { useClerk } from "@clerk/clerk-react";
-
-import "../Button/idea.css";
-import "./ClientChat.css";
 import { useSelector } from "react-redux";
+import "./ClientChat.css";
+import "../Button/idea.css";
 
 const ClientChat = ({ socket, isModalOpen, showChat }) => {
   const { user } = useClerk();
   const userInfo = useSelector((state) => state.users.userInfo);
-  const { id } = useParams();
+  const [id, setId] = useState("");
   const [mensaje, setMensaje] = useState("");
   const [mensajesCliente, setMensajesCliente] = useState(
     JSON.parse(localStorage.getItem("mensajesCliente")) || []
@@ -19,97 +17,82 @@ const ClientChat = ({ socket, isModalOpen, showChat }) => {
 
   const chatContainerRef = useRef(null);
   const inputRef = useRef(null);
-  const [contadorMensajes, setContadorMensajes] = useState(0); // Nuevo estado para contar los mensajes
 
   useEffect(() => {
-    if (showChat && !welcomeSent) {
-      console.log("Uniendo al chat");
-      const mensajeBienvenida =
-        "Good morning, how can we assist you? Live customer service is available from 11am to 4pm.";
-      recibirMensajeServidor(mensajeBienvenida, true);
-      setWelcomeSent(true);
+    if (socket && clientId) {
+      socket.emit("joinClientChat", clientId);
+      socket.on("mensaje_cliente", recibirMensajeServidor);
+      return () => {
+        socket.emit("leaveClientChat", clientId);
+        socket.off("mensaje_cliente", recibirMensajeServidor);
+      };
     }
+  }, [socket, clientId]);
 
-    return () => {
-      if (showChat) {
-        console.log("Desconectando del chat");
-        setWelcomeSent(false);
-      }
-    };
-  }, [showChat]);
+  // useEffect(() => {
+  //  if (showChat && !welcomeSent) {
+  //     const mensajeBienvenida = "Good morning, how can we assist you? Live customer service is available from 11am to 4pm.";
+  //     recibirMensajeServidor(mensajeBienvenida, true);
+  //     setWelcomeSent(true);
+  //  }
+  //  return () => {
+  //     if (showChat) {
+  //       setWelcomeSent(false);
+  //     }
+  //  };
+  // }, [showChat, socket]);
 
   useEffect(() => {
     if (isModalOpen) {
-      inputRef.current.focus();
+      setId(clientId);
     }
-  }, [isModalOpen]);
+  }, [isModalOpen, clientId]);
 
-  const refrescarMensajes = () => {
-    setMensajesCliente([]);
-    localStorage.setItem("mensajesCliente", JSON.stringify([]));
-  };
-
-  const recibirMensajeServidor = (mensajeTexto, esAutomatico = false) => {
-    if (!esAutomatico) {
-      const mensajeTemporal = {
-        tipo: "administrador",
-        clienteId: id,
-        mensaje: { mensaje: "escribiendo...", tiempo: new Date() },
-      };
-      setMensajesCliente((prev) => [...prev, mensajeTemporal]);
-      scrollToBottom();
-
-      setTimeout(() => {
-        setMensajesCliente((prev) => prev.slice(0, -1));
-        agregarMensajeReal(mensajeTexto);
-      }, 1500);
-    } else {
-      agregarMensajeReal(mensajeTexto);
-    }
-  };
-
-  const agregarMensajeReal = (mensajeTexto) => {
-    const mensajeReal = {
-      tipo: "administrador",
-      clienteId: id,
-      mensaje: { mensaje: mensajeTexto, tiempo: new Date() },
-    };
-    setMensajesCliente((prev) => [...prev, mensajeReal]);
+  const recibirMensajeServidor = (mensaje) => {
+    setMensajesCliente((prevMensajesCliente) => [
+      ...prevMensajesCliente,
+      { tipo: "administrador", clienteId: id, mensaje },
+    ]);
     scrollToBottom();
   };
 
   const enviarMensaje = (event) => {
     event.preventDefault();
     if (mensaje.trim() !== "") {
-      // Crear el objeto mensaje con la hora actual
-      const mensajeConTiempo = {
+      const clienteMensaje = {
         tipo: "cliente",
         clienteId: id,
-        mensaje: { mensaje, tiempo: new Date() },
+        mensaje: { mensaje },
       };
-
-      // Agregar el mensaje al estado para actualizar la interfaz de usuario
-      setMensajesCliente((prev) => [...prev, mensajeConTiempo]);
-
-      // Limpiar el campo de entrada de mensaje
-      setMensaje("");
-
-      // Desplazarse a la parte inferior del chat
-      scrollToBottom();
-
-      // Verificar si el socket está definido y conectado antes de enviar el mensaje
-      if (socket && socket.connected) {
-        // Enviar el mensaje al servidor utilizando el socket
-        socket.emit("enviar_mensaje", mensajeConTiempo);
-      } else {
-        console.error("Socket no está conectado.");
+      setMensajesCliente((prevMensajesCliente) => [
+        ...prevMensajesCliente,
+        clienteMensaje,
+      ]);
+      if (socket) {
+        socket.emit("cliente_mensaje", {
+          clienteId: clientId,
+          mensaje: mensaje,
+        });
       }
+      setMensaje("");
+      scrollToBottom();
     }
+  };
+
+  const refrescarMensajes = () => {
+    setMensajesCliente([]);
+    localStorage.setItem("mensajesCliente", JSON.stringify([]));
   };
 
   const scrollToBottom = () => {
     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
   };
+
+  // Función para formatear la fecha desde un timestamp
+  function formatDateFromTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  }
 
   return (
     <div className="max-w-md mx-auto p-4 bg-gray-100 rounded-xl shadow">
@@ -134,11 +117,7 @@ const ClientChat = ({ socket, isModalOpen, showChat }) => {
                 m.tipo === "cliente" ? "timestamp-client" : "timestamp-admin"
               }`}
             >
-              {new Date(m.mensaje.tiempo).toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: true,
-              })}
+              {formatDateFromTimestamp(m.mensaje.tiempo)}
             </div>
           </div>
         ))}
@@ -170,92 +149,3 @@ const ClientChat = ({ socket, isModalOpen, showChat }) => {
 };
 
 export default ClientChat;
-
-// import React, { useState, useEffect, useRef } from 'react';
-// import { useParams } from 'react-router-dom';
-
-// const ClientChat = ({ socket }) => {
-//   const { id } = useParams();
-//   const [mensaje, setMensaje] = useState('');
-//   const [mensajesCliente, setMensajesCliente] = useState(JSON.parse(localStorage.getItem('mensajesCliente')) || []);
-
-//   const clientId = id;
-
-//   const chatContainerRef = useRef(null);
-
-//       localStorage.setItem('mensajesCliente', JSON.stringify(mensajesCliente));
-
-//       const inputRef = useRef(null);
-
-//       useEffect(() => {
-//         inputRef.current.focus();
-//       }, []);
-
-//   useEffect(() => {
-//     if (socket) {
-//       socket.emit('joinClientChat', clientId);
-//       socket.on('mensaje_cliente', recibirMensajeServidor);
-
-//       return () => {
-//       socket.off('mensaje_cliente', recibirMensajeServidor);
-//       };
-//     }
-//   }, [socket, clientId]);
-
-//   const recibirMensajeServidor = (mensaje) => {
-//     setMensajesCliente(prevMensajesCliente => [...prevMensajesCliente, { tipo: 'administrador', clienteId: id, mensaje }]);
-//     scrollToBottom();
-//   };
-
-//   const enviarMensaje = (event) => {
-//     event.preventDefault();
-//     if (socket && mensaje.trim() !== '') {
-//       const clienteMensaje = { tipo: 'cliente', clienteId: id, mensaje: { mensaje } };
-//       setMensajesCliente((prevMensajesCliente) => [...prevMensajesCliente, clienteMensaje]);
-//       socket.emit('cliente_mensaje', {clienteId: clientId, mensaje: mensaje});
-//       setMensaje('');
-//       scrollToBottom();
-//     }
-//   };
-
-//   const refrescarMensajes = () => {
-//     setMensajesCliente([]);
-//     localStorage.setItem('mensajesCliente', JSON.stringify([]));
-//   };
-
-//   const scrollToBottom = () => {
-//     chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
-//   };
-
-//   return (
-//     <div className="max-w-md mx-auto p-4 bg-gray-100 rounded shadow">
-//       <h1 className="text-2xl font-bold mb-4 text-center">Client : {id}</h1>
-
-//       <div ref={chatContainerRef} className="chat-container">
-//         {mensajesCliente.map((mensaje, index) => (
-//           <div key={index} className={`mensaje ${mensaje.tipo === 'cliente' ? 'bg-d' : 'bg-gray-300'} text-black p-2 rounded mb-2`}>
-//            {mensaje.tipo === 'cliente' ? 'Client' : 'admin'}: {mensaje.mensaje.mensaje}
-//           </div>
-//         ))}
-//       </div>
-
-//       <form onSubmit={enviarMensaje} className="flex items-center mt-4">
-
-//         <input
-//         ref={inputRef}
-//         type="text"
-//         value={mensaje}
-//         onChange={(e) => setMensaje(e.target.value)}
-//         placeholder="Escribe tu mensaje aquí..."
-//         className="flex-1 mr-2 p-2 rounded border border-gray-300 focus:outline-none focus:border-green-500"
-//         />
-//         <button type="submit" className="bg-amber-300 hover:bg-amber-400 transition-colors px-4 py-2 rounded focus:outline-none">Send</button>
-//       </form>
-//       <div className="mt-4 flex justify-center">
-//         <button onClick={refrescarMensajes} className="w-40 bg-amber-300 hover:bg-amber-400 transition-colors text-white font-bold py-2 px-4 rounded-2xl focus:outline-none focus:shadow-outline">REFRESH CHAT</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ClientChat;
