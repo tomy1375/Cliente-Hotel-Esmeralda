f8125826694ab85a6ae15b09fde9593f683da38e
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-21 20:56:53 -0600

    Cambio backend

--- src/utils/global.js
+++ src/utils/global.js
@@ -3,7 +3,7 @@ const APP_ENV = "PRODUCTION"; //PRODUCTION O LOCAL
 let API_URL = "https://backend-hotelesmeralda.onrender.com/";
 
 if (APP_ENV === "PRODUCTION") {
-  API_URL = "https://backend-pf-web16pt.onrender.com";
+  API_URL = "https://backend-pf-web16pt.onrender.com/";
 } else if (APP_ENV === "LOCAL") {
   API_URL = "http://localhost:4000/";
 }
