0e321bd69f66b9643004728a6324570ea04604b2
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-04-21 18:35:38 -0600

    Produccion

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,14 +1,11 @@
-const APP_ENV = 'PRODUCTION';  //PRODUCTION O LOCAL
+const APP_ENV = "PRODUCTION"; //PRODUCTION O LOCAL
 
-let API_URL = 'https://backend-hotelesmeralda.onrender.com/';
+let API_URL = "https://backend-hotelesmeralda.onrender.com/";
 
-if (APP_ENV === 'PRODUCTION') {
-  API_URL = 'https://backend-hotelesmeralda.onrender.com/';
-} else if (APP_ENV === 'LOCAL') {
-  API_URL = 'http://localhost:4000/';
+if (APP_ENV === "PRODUCTION") {
+  API_URL = "https://backend-hotelesmeralda.onrender.com/";
+} else if (APP_ENV === "LOCAL") {
+  API_URL = "http://localhost:4000/";
 }
 
-export {
-    APP_ENV,
-    API_URL,
-}
+export { APP_ENV, API_URL };
