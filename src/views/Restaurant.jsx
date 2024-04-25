315c4b0f2dc21177ad314fc21384df40323cf919
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-04-25 04:35:18 -0300

    restaurant

--- src/views/Restaurant.jsx
+++ src/views/Restaurant.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
-import MenuPage from '../components/restaurant/Restaurant'
-import Food from '../components/restaurant/RestaurantFood'
+import MenuPage from '../components/restaurant/Restaurant';
+import Food from '../components/restaurant/RestaurantFood';
 import ReservationForm from '../components/restaurant/ReservationForm';
 import { useLocation } from 'react-router-dom';
 
 const Restaurant = () => {
-   const [currentPage, setCurrentPage] = useState(1);
-   const location = useLocation();
+ const [currentPage, setCurrentPage] = useState(1);
+ const location = useLocation();
 
-  useEffect(() => {
+ useEffect(() => {
     // Desplazamiento automático a la sección del formulario si el hash está presente en la URL
     if (location.hash) {
       const id = location.hash.replace("#", "");
@@ -17,26 +17,25 @@ const Restaurant = () => {
         element.scrollIntoView({ behavior: 'smooth' });
       }
     }
-  }, [location]); // Dependiendo de la ubicación para reaccionar a cambios
-  return (
-      <div className="flex flex-col items-center mt-16 mb-8">
-   <h1 id="food" className="text-6xl items-center font-extrabold text-center max-w-[548px] text-neutral-800 tracking-[3px] max-md:text-4xl">
-      We serve passion.
-      An oasis of pleasure...
-   </h1>
-   <div  className="flex justify-start">
-      <MenuPage setCurrentPage={setCurrentPage}/>
-      <div className='items-end'>
+ }, [location]); // Dependiendo de la ubicación para reaccionar a cambios
 
-      <Food currentPage={currentPage} setCurrentPage={setCurrentPage} />
+ return (
+    <div className="flex flex-col items-center mt-16 mb-8">
+      <h1 id="food" className="text-6xl items-center font-extrabold text-center max-w-[548px] text-neutral-800 tracking-[3px] max-md:text-4xl">
+        We serve passion.
+        An oasis of pleasure...
+      </h1>
+      <div className="flex justify-start">
+        <MenuPage setCurrentPage={setCurrentPage} />
+        <div className='items-end'>
+          <Food currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </div>
       </div>
-   </div>
-   <div id="reservation-form" className='m-10 w-full'>
-        <ReservationForm/>
+      <div id="reservation-form" className='m-10 w-full'>
+        <ReservationForm />
       </div>
-   </div>
+    </div>
+ );
+};
 
-  )
-}
-
-export default Restaurant
+export default Restaurant;
